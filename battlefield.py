4765c6e2b4c596359eb99c89d1a76d651962d5ce
Fuechee <f9vang@gmail.com>
2022-08-11 14:48:09 -0500

    Fixed robot and finished coding battlefield to complete the project.

--- battlefield.py
+++ battlefield.py
@@ -3,22 +3,31 @@ from dinosaur import Dinosaur
 
 class Battlefield:
     def __init__(self):
-        self.robot = Robot()
-        self.dinosaur = Dinosaur()
+        self.robot = Robot('Orix')
+        self.dinosaur = Dinosaur('Godzilla', 35)
     
     def run_game(self):
-        self.display_welcome(self)
-        self.battle_phase(self)
-        self.display_winner(self)
+        self.display_welcome()
+        self.battle_phase()
+        self.display_winner()
 
 
     def display_welcome(self):
-        pass
+        print('\nWelcome to Robot VS Dinosaur\nWho will Win!\n')
 
     def battle_phase(self):
-        pass
+        while(self.robot.health >0 and self.dinosaur.health >0):
+            self.robot.attack(self.dinosaur)
+            print(f'{self.robot.name} attacked {self.dinosaur.name} with {self.robot.active_weapon.name} for {self.robot.active_weapon.attack_power} damage!!')
+            print(f'{self.dinosaur.name} has {self.dinosaur.health} health remaining!')
+            if(self.dinosaur.health >0):
+                self.dinosaur.attack(self.robot)
+                print(f'{self.dinosaur.name} attacked {self.robot.name} for {self.dinosaur.attack_power} damage!!!')
+                print(f'{self.robot.name} has {self.robot.health} health remaining!')
 
     def display_winner(self):
-        if len(self.robot) == 0:
-            print('')
-        
+        if(self.robot.health <= 0):
+            print(f'{self.dinosaur.name} is the WINNER!!!')
+        elif(self.dinosaur.health <= 0):
+            print(f'{self.robot.name} is the WINNER!!!!')
+            
--- robot.py
+++ robot.py
@@ -4,9 +4,12 @@ class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 100
-        self.active_weapon = Weapon(30)
+        self.active_weapon = Weapon('Laser Cannon', 35)
     
     def attack(self, dinosaur):
         dinosaur.health -= self.active_weapon.attack_power
+
+
+       
         
 
